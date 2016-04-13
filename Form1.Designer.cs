ceca282049d2e0d9cc67000d28fbf0d806d7923d
Mohamed Oumechtak <oumechtakmohamed94@gmail.com>
2016-04-13 16:42:17 +0200

    second commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.gameover_lbl = new System.Windows.Forms.Label();
+            this.lbl_points = new System.Windows.Forms.Label();
+            this.score_lbl = new System.Windows.Forms.Label();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Rackette = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rackette)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.gameover_lbl);
+            this.playground.Controls.Add(this.lbl_points);
+            this.playground.Controls.Add(this.score_lbl);
+            this.playground.Controls.Add(this.Ball);
+            this.playground.Controls.Add(this.Rackette);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(714, 403);
+            this.playground.TabIndex = 0;
+            // 
+            // gameover_lbl
+            // 
+            this.gameover_lbl.AutoSize = true;
+            this.gameover_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameover_lbl.Location = new System.Drawing.Point(190, 103);
+            this.gameover_lbl.Name = "gameover_lbl";
+            this.gameover_lbl.Size = new System.Drawing.Size(351, 220);
+            this.gameover_lbl.TabIndex = 4;
+            this.gameover_lbl.Text = "Game Over\r\n\r\nEnter - Restart\r\nEsc - Exit";
+            this.gameover_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_points
+            // 
+            this.lbl_points.AutoSize = true;
+            this.lbl_points.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_points.Location = new System.Drawing.Point(153, 0);
+            this.lbl_points.Name = "lbl_points";
+            this.lbl_points.Size = new System.Drawing.Size(52, 55);
+            this.lbl_points.TabIndex = 3;
+            this.lbl_points.Text = "0";
+            // 
+            // score_lbl
+            // 
+            this.score_lbl.AutoSize = true;
+            this.score_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score_lbl.Location = new System.Drawing.Point(3, 0);
+            this.score_lbl.Name = "score_lbl";
+            this.score_lbl.Size = new System.Drawing.Size(183, 55);
+            this.score_lbl.TabIndex = 2;
+            this.score_lbl.Text = "Score: ";
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Lime;
+            this.Ball.Location = new System.Drawing.Point(143, 76);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(30, 30);
+            this.Ball.TabIndex = 1;
+            this.Ball.TabStop = false;
+            // 
+            // Rackette
+            // 
+            this.Rackette.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Rackette.Location = new System.Drawing.Point(239, 340);
+            this.Rackette.Name = "Rackette";
+            this.Rackette.Size = new System.Drawing.Size(200, 20);
+            this.Rackette.TabIndex = 0;
+            this.Rackette.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 403);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rackette)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.PictureBox Rackette;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_points;
+        private System.Windows.Forms.Label score_lbl;
+        private System.Windows.Forms.Label gameover_lbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        public int speed_left = 4; //speed of the ball
+        public int speed_top = 4;
+        public int point = 0; //score
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+            Cursor.Hide(); // cacher le curseur
+            this.FormBorderStyle = FormBorderStyle.None; // désactive toute autre bordure
+            this.TopMost = true; // faire intervenir the form to the front
+            this.Bounds = Screen.PrimaryScreen.Bounds; // make it fullscreen
+
+            Rackette.Top = playground.Bottom - (playground.Bottom / 10); // positionner la rackette
+
+            gameover_lbl.Left = (playground.Width / 2) - (gameover_lbl.Width / 2);
+            gameover_lbl.Top = (playground.Height / 2) - (gameover_lbl.Height / 2);
+            gameover_lbl.Visible = false;
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Rackette.Left = Cursor.Position.X - (Rackette.Width / 2);
+            Ball.Left += speed_left; // move the ball
+            Ball.Top += speed_top;
+
+            if (Ball.Bottom >= Rackette.Top && Ball.Bottom <= Rackette.Bottom && Ball.Left >= Rackette.Left && Ball.Right <= Rackette.Right) // rackette collision
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top; // changer la direction
+                point += 1;
+                lbl_points.Text = point.ToString();
+                Random r = new Random();
+                playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+            }
+
+            if (Ball.Left <= playground.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (Ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (Ball.Top <= playground.Top)
+            {
+                speed_top = -speed_top;
+            }
+
+            if (Ball.Bottom >= playground.Bottom)
+            {
+                timer1.Enabled = false; // ball is out stop the game
+                gameover_lbl.Visible = true;
+            }
+        }
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) { this.Close(); } // appuyer sur eshape pour fermer
+            if (e.KeyCode == Keys.Enter)
+            {
+                Ball.Top = 50;
+                Ball.Left = 50;
+                speed_left = 4;
+                speed_top = 4;
+                point = 4;
+                lbl_points.Text = "0";
+                timer1.Enabled = true;
+                gameover_lbl.Visible = false;
+                playground.BackColor = Color.White;
+
+
+
+            }
+        }
+    }
+}
+        
